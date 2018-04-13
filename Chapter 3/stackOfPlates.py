60cc029913d00b1839927d2a22ac19b4df899b82
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-13 13:58:44 -0700

    indetation error is magically fixed, gettting attribute error now

--- Chapter 3/stackOfPlates.py
+++ Chapter 3/stackOfPlates.py
@@ -11,24 +11,21 @@ class stackOfPlates:
 		p = Stack()
 		if ((self.isEmpty()) or (self.plates[len(self.plates)-1].size() == cap)):
 			p.push(item)
-        	self.plates.append(p)
+			self.plates.append(p)
 		else:
-    		self.plates[len(self.plates)-1].push(item)
+			self.plates[len(self.plates)-1].push(item)
 
 	def pop(self):
-    	temp = self.plates[len(self.plates)-1].pop()
-    	if(self.plates[len(self.plates)-1].isEmpty()): self.plates.pop()
-    	return temp
-    	
-    def peek(self):
-    	return self.plates[len(self.plates)-1].peek()
+		temp = self.plates[len(self.plates)-1].pop()
+		if(self.plates[len(self.plates)-1].isEmpty()): self.plates.pop()
+		return temp
+		
+	def peek(self):
+		return self.plates[len(self.plates)-1].peek()
 
-    def size(self):
-    	return len(self.plates)
+	def size(self):
+		return len(self.plates)
 
 s = stackOfPlates()
 s.push(3)
-s.push(5)
-s.push(1)
-s.push(5)
-s.push(2)
+
