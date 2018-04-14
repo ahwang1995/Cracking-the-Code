b58d6a326e4c836b1894f387607eb2aa2c71b109
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-14 13:43:07 -0700

    moved inorder to method in Node and added preorder

--- Chapter 4/inOrder.py
+++
@@ -1,7 +0,0 @@
-from Node import Node
-class inOrder:
-	def inOrderTraversal(node):
-		if node != None:
-			self.inOrderTraversal(node.left);
-			print (node.data)
-			self.inOrderTraversal(node.right)
