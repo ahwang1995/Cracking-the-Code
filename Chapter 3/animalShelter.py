ee5b7d458dd949554f29f0b71147131bb604883a
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-14 01:40:14 -0700

    comments

--- Chapter 3/animalShelter.py
+++ Chapter 3/animalShelter.py
@@ -7,6 +7,7 @@ class animalShelter:
 		self.cats = Queue()
 	#timestamp can be done using tuples, but I just queued the timestamp at the end
 	def enqueue(self,animal,which):
+		#check if dog or cat and enqueue with timestamp
 		if which == "cat":
 			self.cats.enqueue(time.time())
 			self.cats.enqueue(animal)
@@ -14,35 +15,46 @@ class animalShelter:
 		if which == "dog":
 			self.dogs.enqueue(time.time())
 			self.dogs.enqueue(animal)
-
+	#dequeue regardless of dog or cat
 	def dequeueAny(self):
+		#if both queues empty return false
 		if (self.cats.isEmpty()) and (self.dogs.isEmpty()):
 			return False
+
+		#if cats is empty dequeue dog
 		if (self.cats.isEmpty()) and (not self.dogs.isEmpty()):
 			self.dogs.dequeue()
 			dog = self.dogs.dequeue()
 			return dog
+
+		#if dogs is empty dequeue cat
 		if (self.dogs.isEmpty()) and (not self.cats.isEmpty()):
 			self.cats.dequeue()
 			cat = self.cats.dequeue()
 			return cat
+
+		#compare dog and cat timestamps and dequeue oldest
 		d = self.dogs.peek()
 		c = self.cats.peek()
 		if(c < d):
 			self.cats.dequeue()
 			cat = self.cats.dequeue()
 			return cat
+
 		else:
 			self.dogs.dequeue()
 			dog = self.dogs.dequeue()
 			return dog
 
+	#dequeue dog
 	def dequeueDog(self):
 		if(self.dogs.isEmpty()):
 			return False
 		self.dogs.dequeue()
 		dog = self.dogs.dequeue()
 		return dog
+
+	#dequeue cat
 	def dequeueCat(self):
 		if(self.cats.isEmpty()):
 			return False
