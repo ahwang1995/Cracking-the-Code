4c2724974fa7159f132fcbeb524e9a58dc1f5077
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-15 18:17:34 -0700

    finished and rested list of depths

--- Chapter 4/listOfDepths.py
+++ Chapter 4/listOfDepths.py
@@ -1,7 +1,8 @@
 from Node import Node
 from Queue import Queue
 from collections import deque
-class listofDepths:
+from minimalTree import minimalTree
+class listOfDepths:
 	def __init__(self):
 		self.depthList = []
 
@@ -14,11 +15,12 @@ class listofDepths:
 		#track the number of nodes visited so far
 		currDepth = 0
 		rootList = deque()
-		depthList.append(dequeue.append(root))
+		rootList.append(root)
+		self.depthList.append(rootList)
 		while(not q.isEmpty()):
 			r = q.dequeue()
 			if r.depth > currDepth:
-				currDepth = currdepth + 1
+				currDepth = currDepth + 1
 				nextList = deque()
 				nextList.append(r)
 				self.depthList.append(nextList)
@@ -26,10 +28,23 @@ class listofDepths:
 				self.depthList[len(self.depthList)-1].append(r)
 
 			#print(r.data)
-
 			if r.left:
 				r.left.depth = currDepth + 1
 				q.enqueue(r.left)
 			if r.right:
 				r.right.depth = currDepth + 1
 				q.enqueue(r.right)
+		return self.depthList
+
+#testing
+a = [1,2,3,4,8,22,34,38,54]
+mT = minimalTree()
+n = mT.createBinTree(a)
+#n.inOrder(n)
+lod = listOfDepths()
+depthLists = lod.makeList(n)
+print depthLists[0][0].data
+print depthLists[1][0].data
+print depthLists[1][1].data
+print depthLists[2][2].data
+print depthLists[2][3].data
