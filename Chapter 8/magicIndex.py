08404e4724ccc689a79636864fea18dab7c82aa2
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-19 20:13:23 -0700

    magic index for sorted nondistinct elements

--- Chapter 8/magicIndex.py
+++ Chapter 8/magicIndex.py
@@ -4,7 +4,7 @@
 class magicIndex:
 	#returns the magic index if there is one
 	def magInd(self,arr):
-		return self.searchIndDist(arr,0,len(arr)-1)
+		return self.searchInd(arr,0,len(arr)-1)
 	#similar to binary search, find the magic index with a distinct array
 	def searchIndDist(self,arr,start,end):
 		if(end < start):
@@ -21,7 +21,26 @@ class magicIndex:
 		else:
 			return self.searchIndDist(arr,mid + 1,end)
 
+	#if there is a sorted array of nondistinct elements
+	def searchInd(self,arr,start,end):
+		if(end < start):
+			return -1
+
+		mid = (start+end)/2
+
+		if(arr[mid] == mid):
+			return mid
+
+		leftInd = min(mid-1,arr[mid])
+		left = self.searchInd(arr,start,leftInd)
+		print left
+		if(left >= 0):
+			return left
+		rightInd = min(mid+1,arr[mid])
+		right = self.searchInd(arr,rightInd,end)
+		return right
+
 #test
 mi = magicIndex()
-arr = [-5,-3,0,1,3,5,6,9,11,15]
+arr = [1,2,2]
 print mi.magInd(arr)
