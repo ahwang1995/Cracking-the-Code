85aab0e98862273eb1b882b8a859cc6666977ece
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-19 18:44:16 -0700

    started magicIndex

---
+++ Chapter 8/magicIndex.py
@@ -0,0 +1,9 @@
+#given a sorted array find the magic index, which is when the
+#element is equal to it's index
+#do this for the case where the array has distinct element and if it doesn't
+class magicIndex:
+	#returns the magic index if there is one
+	def magInd(self,arr):
+
+	#similar to binary search, find the magic index
+	def searchInd(self,arr,start,end):
