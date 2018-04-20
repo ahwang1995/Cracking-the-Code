6857a5ea6ccdf64c51853e222ef54bafce140b78
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-19 19:48:10 -0700

    finished magic index given distinct elements in an array

--- Chapter 8/magicIndex.py
+++ Chapter 8/magicIndex.py
@@ -4,6 +4,24 @@
 class magicIndex:
 	#returns the magic index if there is one
 	def magInd(self,arr):
+		return self.searchIndDist(arr,0,len(arr)-1)
+	#similar to binary search, find the magic index with a distinct array
+	def searchIndDist(self,arr,start,end):
+		if(end < start):
+			return False
 
-	#similar to binary search, find the magic index
-	def searchInd(self,arr,start,end):
+		mid = (start+end)/2
+
+		if(arr[mid] == mid):
+			return mid
+
+		elif(arr[mid] > mid):
+			return self.searchIndDist(arr,start,mid-1)
+
+		else:
+			return self.searchIndDist(arr,mid + 1,end)
+
+#test
+mi = magicIndex()
+arr = [-5,-3,0,1,3,5,6,9,11,15]
+print mi.magInd(arr)
