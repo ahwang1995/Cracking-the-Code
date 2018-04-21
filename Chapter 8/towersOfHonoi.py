3e636c1d1ffb944b7302f549abe3c385e0f050da
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-20 17:28:29 -0700

    finished towers of Honoi

--- Chapter 8/towersOfHonoi.py
+++ Chapter 8/towersOfHonoi.py
@@ -4,10 +4,27 @@ from Stack import Stack
 #each tower is represented by a stack and larger discs cannot be set
 #on top of larger discs, only one disc can be moved at a time, and only the top can be moved
 class towersOfHanoi:
-	#initialize the three towers (goal is to move all elements to tower 3)
-	def __init__(self,tower1):
-		self.tower1 = tower1
-		self.tower2 = Stack()
-		self.tower3 = Stack()
+	#recursive function to move discs from a to b using c
+	def moveDiscs(self,n,a,b,c):
+		#base case
+		if n == 1: return
 
-	def moveDiscs(self,):
+		#move rods 1 by 1 using recursion
+		self.moveDiscs(n-1,a,b,c)
+		if not a.isEmpty():
+			b.push(a.pop())
+		self.moveDiscs(n-1,c,b,a)
+
+a = Stack()
+b = Stack()
+c = Stack()
+a.push(1)
+a.push(2)
+a.push(3)
+a.push(4)
+n = a.size()
+toh = towersOfHanoi()
+toh.moveDiscs(n,a,b,c)
+
+while not b.isEmpty():
+	print(b.pop())
