27c42fbf5afe10d8003bb111cc0af03af410ea06
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-23 13:56:52 -0700

    finished testing

--- Chapter 8/stackOfBoxes.py
+++ Chapter 8/stackOfBoxes.py
@@ -16,31 +16,46 @@ class stackOfBoxes:
 	def tallestStack(self,boxArr):
 		sortedBoxes = sorted(boxArr,key = lambda box: box.height, reverse = True)
 		maxHeight = 0
-		stackMap = []*len(boxArr)
-		return makeStack(boxArr,None,0,stackMap)
+		stackMap = [0]*len(boxArr)
+		return self.makeStack(boxArr,None,0,stackMap)
 
 	#recursive function to build stacks, sort by 1 paramenter since it needs to be strictly greater
 	def makeStack(self,boxArr,bottom,offset,stackMap):
 		#base case
 		if offset >= len(boxArr): return 0
 
-		currBot = boxArr(offset)
+		currBot = boxArr[offset]
 		heightWithBot = 0
 		if bottom == None or currBot.striclyGreater(bottom):
 			if stackMap[offset] == 0:
-				stacMap[offset] = makeStack(boxArr,currBot,offset + 1,stackMap)
+				stackMap[offset] = self.makeStack(boxArr,currBot,offset + 1,stackMap)
 				stackMap[offset] = stackMap[offset] + currBot.height
 			heightWithBot = stackMap[offset]
 
 		#with a different bottom
-		heightWithoutBot = makeStack(boxArr,bottom,offset + 1, stackMap)
+		heightWithoutBot = self.makeStack(boxArr,bottom,offset + 1, stackMap)
 		return max(heightWithBot,heightWithoutBot)
 
 #test
 boxArr = []
 for i in xrange(0,5):
-	x = box(i,i,i)
+	x = box()
 	boxArr.append(x)
+boxArr[0].height = 1
+boxArr[0].width = 2
+boxArr[0].length = 3
+boxArr[1].height = 3
+boxArr[1].width = 2
+boxArr[1].length = 1
+boxArr[2].height = 4
+boxArr[2].width = 4
+boxArr[2].length = 4
+boxArr[3].height = 5
+boxArr[3].width = 6
+boxArr[3].length = 7
+boxArr[4].height = 7
+boxArr[4].width = 6
+boxArr[4].length = 5
 
-
-a = sorted(boxArr,key=lambda box: box.height, reverse = True)
+sob = stackOfBoxes()
+print sob.tallestStack(boxArr)
