51b045afec2b5a9deefba3204ca8d8719a6ad886
Andrew Hwang <auhwang@andrews-mbp.attlocal.net>
2018-04-26 17:19:04 -0700

    found an error while testing with duplicate values

--- Chapter 10/rotatedArraySearch.py
+++ Chapter 10/rotatedArraySearch.py
@@ -7,18 +7,22 @@ class rotatedArraySearch:
 		#check which side is ordered properly
 		if arr[low] < arr[mid]:
 			#if the left side is ordered properly, check if it is in the left side, if not search right
-			if x >= a[low] and x < a[mid]: return self.rotatedAS(arr,x,low,mid-1)
-			else: return self,rotatedAS(arr,x,mid+1,high)
+			if x >= arr[low] and x < arr[mid]: return self.rotatedAS(arr,x,low,mid-1)
+			else: return self.rotatedAS(arr,x,mid+1,high)
 		elif arr[mid] < arr[low]:
 			#same for right side
-			if x > arr[mid] and a <= arr[high]: return self.rotatedAS(arr,x,mid+1,high)
-			else: return self.rotatesAS(arr,x,low,mid-1)
+			if x > arr[mid] and arr <= arr[high]: return self.rotatedAS(arr,x,mid+1,high)
+			else: return self.rotatedAS(arr,x,low,mid-1)
 		#if there are duplicate values so the order is not known, check both sides
-		elif a[low] == a[mid]:
-			if a[mid] != a[right]: return self.rotatedAS(a,mid+1,high,x)
+		elif arr[low] == arr[mid]:
+			if arr[mid] != arr[high]: return self.rotatedAS(arr,mid+1,high,x)
 			else:
-				result = self.rotatedAS(a,low,mid-1,x)
-				if(result == False): return self.rotatedAS(a,mid+1,high,x)
+				result = self.rotatedAS(arr,low,mid-1,x)
+				if(result == False): return self.rotatedAS(arr,mid+1,high,x)
 				else: return result
 		return False
 
+arr = [5,6,7,2,3,4]
+arr2 = [2,2,2,2,3,4,1]
+ras = rotatedArraySearch()
+print ras.rotatedAS(arr2,3,0,len(arr2)-1)
